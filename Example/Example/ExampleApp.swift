dee423d7a64d337c3842c5ea938241c7d4ef1940
Yonghwi <yonghwi.nam@gmail.com>
2024-08-29 14:49:03 +0900

    add example project

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Yonghwi on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Example/ExampleTests/ExampleTests.swift
@@ -0,0 +1,16 @@
+//
+//  ExampleTests.swift
+//  ExampleTests
+//
+//  Created by Yonghwi on 8/29/24.
+//
+
+import Testing
+
+struct ExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
